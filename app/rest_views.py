5f6a948fc69f04092c858db649bd01074df150fd
Emmet Hanratty <emmethanratty@hotmail.com>
2017-04-27 15:51:51 +0100

    added test

--- app/rest_views.py
+++ app/rest_views.py
@@ -117,4 +117,4 @@ def token_login(request):
 
 def test(request):
     print('test worked')
-    return Response({"detail":"Just testing"}, status=status.HTTP_200_OK)
+    return Response({"detail": "Just testing"}, status=status.HTTP_200_OK)
